5bc7b1315b32fd900d6c1abea338029d2dab6218
vlad ghioc <vladghiocel@gmail.com>
2022-03-30 19:18:29 +0200

    tema_011 crypto +responsive till 500px

---
+++ tema_011 crypto/scripts/script.js
@@ -0,0 +1,27 @@
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+    var data = google.visualization.arrayToDataTable([
+        ['Year', 'Sales', 'Expenses'],
+        ['2004',  1000,      400],
+        ['2005',  1170,      460],
+        ['2006',  660,       1120],
+        ['2007',  1030,      540]
+    ]);
+
+    var options = {
+        title: 'Total Portfolio',
+        curveType: 'function',
+        legend: {position: 'none'},
+       tooltips: {
+           enabled: false
+        },
+    };
+
+    var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+
+    chart.draw(data, options);
+
+
+}
